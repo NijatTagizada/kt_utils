d00bde3b049f767f3a43ead1d5d97819c1c2cc90
NijatTagizada <tagizada.nicat@gmail.com>
2021-12-01 01:39:25 +0400

    init

---
+++ example/main.dart
@@ -0,0 +1,5 @@
+import 'package:kt_utils/kt_utils.dart';
+
+void main(List<String> args) {
+
+}
---
+++ lib/kt_utils.dart
@@ -0,0 +1,7 @@
+library kt_utils;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
